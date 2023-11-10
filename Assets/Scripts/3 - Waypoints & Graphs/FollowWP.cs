130ce74076819718cdc176c6de51db7bac720207
kgkghghkj <126677099+Fabio-Maia-1@users.noreply.github.com>
2023-11-10 22:04:51 +0000

    Etapa 2 (Following a Circuit&Tracker) do tutorial 3 (Waypoints & Graphs)
    Completei a etapa "Following a Circuit&Tracker" do tutorial "Waypoints & Graphs". Fiz todos os objetivos na mesma scene que a etapa anterior (Waypoints), alterando então o nome da scene para "Waypoints + Following a Circuit&Tracker".

--- Assets/Scripts/3 - Waypoints & Graphs/FollowWP.cs
+++ Assets/Scripts/3 - Waypoints & Graphs/FollowWP.cs
@@ -9,31 +9,45 @@ public class FollowWP : MonoBehaviour
 
     public float speed = 10.0f;
     public float rotSpeed = 1.0f;
+    public float lookAhead = 10.0f;
+
+    GameObject tracker;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<Collider>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProgressTracker()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
         {
             currentWP++;
         }
 
         if (currentWP >= waypoints.Length)
         {
-            currentWP= 0;
+            currentWP = 0;
         }
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0,(speed+0.01f)*Time.deltaTime);
+    }
 
-        //this.transform.LookAt(waypoints[currentWP].transform.position);
+    // Update is called once per frame
+    void Update()
+    {
+        ProgressTracker();
 
-        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        Quaternion lookatWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
 
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, Time.deltaTime);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
 
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
