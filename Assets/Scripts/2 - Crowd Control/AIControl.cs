fff2ef8809493b5546d8af6da8243f400d5a6a30
kgkghghkj <126677099+Fabio-Maia-1@users.noreply.github.com>
2023-11-10 17:04:36 +0000

    Etapa 3 (Fleeing) do tutorial 2 (Crowd Simulation)
    Acabei a etapa "Fleeing" do tutorial "Crowd Simulation". Criei uma pasta scene chamada "Fleeing" e coluquei-a na pasta "2 - Crowd Simulation". Para criar um obstáculo no cenário, apenas é necessário clicar com o rato em qualquer superfície.

--- Assets/Scripts/2 - Crowd Control/AIControl.cs
+++ Assets/Scripts/2 - Crowd Control/AIControl.cs
@@ -8,26 +8,56 @@ public class AIControl : MonoBehaviour
     GameObject[] goalLocations;
     NavMeshAgent agent;
     Animator anim;
+    float speedMult;
+    float detectionRadius = 20f;
+    float fleeRadius = 10f;
 
     void Start() 
     {
-        agent = this.GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
         goalLocations = GameObject.FindGameObjectsWithTag("goal");
         int i = Random.Range(0, goalLocations.Length);
         agent.SetDestination(goalLocations[i].transform.position);
-        anim = GetComponent<Animator>();
-        anim.SetTrigger("isWalking");
+        anim = this.GetComponent<Animator>();   
         anim.SetFloat("wOffset", Random.Range(0f, 1f));
-        float sm = Random.Range(0.5f, 2f);
-        anim.SetFloat("speedMult", sm);
-        agent.speed *= sm;
+        ResetAgent();
     }
 
+    void ResetAgent()
+    {
+        speedMult = Random.Range(0.5f, 1.5f);
+        anim.SetFloat("speedMult", speedMult);
+        agent.speed *= speedMult;
+        anim.SetTrigger("isWalking");
+        agent.angularSpeed = 120;
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newgoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newgoal, path);
+
+            if (path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10f;
+                agent.angularSpeed = 500f;
+            }
+        }
+    }
 
     void Update()
     {
-        if (agent.remainingDistance < 2)
+        if (agent.remainingDistance < 2.0f)
         {
+            ResetAgent();
             int i = Random.Range(0, goalLocations.Length);
             agent.SetDestination(goalLocations[i].transform.position);
         }
