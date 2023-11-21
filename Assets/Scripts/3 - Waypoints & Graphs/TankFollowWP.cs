3cde18998fb22254afe4d808a4e82bae06e574b0
kgkghghkj <126677099+Fabio-Maia-1@users.noreply.github.com>
2023-11-21 21:44:02 +0000

    Etapa 6 (Commanding a Path) do tutorial 3 (Waypoints & Graphs)
    Completei os objetivos da etapa na mesma scene das etapas anteriores, alterando o nome da scene de "Using AStar with Waypoints Part 2" para "Commanding a Path". Foram acrescentados butões clicáveis ao ecrã.

--- Assets/Scripts/3 - Waypoints & Graphs/TankFollowWP.cs
+++ Assets/Scripts/3 - Waypoints & Graphs/TankFollowWP.cs
@@ -6,7 +6,7 @@ public class TankFollowWP : MonoBehaviour
 {
     Transform goal;
     float speed = 5.0f;
-    float accuracy = 1f;
+    float accuracy = 5f;
     float rotSpeed = 2f;
 
     public GameObject wpManager;
@@ -18,9 +18,12 @@ public class TankFollowWP : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 2.5f;
         wps = wpManager.GetComponent<WPManager>().waypoints;
         g = wpManager.GetComponent<WPManager>().graph;
         currentNode = wps[0];
+
+        //Invoke("GoToRuin", 1);
     }
 
     public void GoToHeli()
@@ -33,6 +36,43 @@ public class TankFollowWP : MonoBehaviour
     {
         g.AStar(currentNode, wps[6]);
         currentWP = 0;
+    }
+
+    public void GotoFactory()
+    {
+
+        g.AStar(currentNode, wps[8]);
+        currentWP = 0;
+    }
+
+    void LateUpdate()
+    {
+        if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
+        {
+            return;
+        }
+
+        //currentNode = g.getPathPoint(currentWP);
+
+        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, this.transform.position) < accuracy)
+        {
+            currentNode = g.pathList[currentWP].getId();
+            currentWP++;
+        }
+
+        if (currentWP < g.pathList.Count)
+        { 
+            goal = g.pathList[currentWP].getId().transform;
+
+            Vector3 lookAtGoal = new Vector3(goal.position.x,this.transform.position.y,goal.position.z);
+
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime * rotSpeed);
+
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+
+        }
     }
 
 
